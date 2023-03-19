920191950e4beb90f0427a6e3d6d5fb2a38e99e2
yujiseok <koesijuy@gmail.com>
2023-03-19 22:41:05 +0900

    refactor: 불필요 키 제거

--- src/components/table/Th.tsx
+++ src/components/table/Th.tsx
@@ -1,13 +1,6 @@
 import type { THeader } from "./Table";
 
 const Th = ({ header }: { header: THeader }) => {
-  return (
-    <th
-      key={header}
-      className={`px-6 py-4 ${header === ("주문상태" || "고객번호") ? "" : ""}`}
-    >
-      {header}
-    </th>
-  );
+  return <th className="px-6 py-4">{header}</th>;
 };
 export default Th;
