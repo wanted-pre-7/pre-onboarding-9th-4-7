d1fbe83ec20a3c7f0ba4c27dddff83d4bb1284ec
Haesol <osunpine@gmail.com>
2023-03-20 21:28:43 +0900

    feat[#4]: 오늘의 거래건 구현
    - 2023-03-08의 거래건만 노출되도록 데이터 필터링을 하였습니다.
    - data는 useMemo를 이용해 메모이제이션하였습니다.
    - 테이블의 길이가 data의 양에 따라 달라 유저 편의성을 위해 스크롤 위치를 고정하였습니다.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,26 +5,34 @@ import Table from "../components/Table";
 import useData from "../hooks/useData";
 
 const Main = () => {
+  const today = "2023-03-08";
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = searchParams.get("page");
-  const query = useData();
   const firstIdx = 50 * (Number(currentPage) - 1);
   const lastIdx = firstIdx + 50;
-  const { data, currentData } = useMemo(
-    () => ({
-      data: query.data?.data,
-      currentData: query.data?.data.slice(firstIdx, lastIdx),
-    }),
+
+  const query = useData();
+  const data = useMemo(
+    () =>
+      query.data?.data.filter(
+        (el) => el.transaction_time.substring(0, 10) === today,
+      ),
     [query],
   );
+  const currentData = useMemo(() => data?.slice(firstIdx, lastIdx), [data]);
 
   useEffect(() => {
     searchParams.set("page", "1");
     setSearchParams(searchParams);
   }, []);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [currentPage]);
+
   return (
     <div className="page-container">
+      <Pagination data={data ? data : []} />
       <Table data={currentData} />
       <Pagination data={data ? data : []} />
     </div>
