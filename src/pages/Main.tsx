bc18b5f6cfa46334cf96a1261e561225a71133c1
Haesol <osunpine@gmail.com>
2023-03-21 01:24:42 +0900

    feat[#7]: 주문 처리 상태 필터링
    - useSearchParams()를 이용해 주문 처리 상태에 따른 필터링을 구현하였습니다.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -8,17 +8,25 @@ const Main = () => {
   const today = "2023-03-08";
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = searchParams.get("page");
+  const status = searchParams.get("status");
   const firstIdx = 50 * (Number(currentPage) - 1);
   const lastIdx = firstIdx + 50;
 
   const query = useData();
-  const data = useMemo(
+  let data = useMemo(
     () =>
       query.data?.data.filter(
         (el) => el.transaction_time.substring(0, 10) === today,
       ),
     [query],
   );
+
+  if (status === "완료") {
+    data = data?.filter((el) => el.status);
+  } else if (status === "미완료") {
+    data = data?.filter((el) => !el.status);
+  }
+
   const currentData = useMemo(() => data?.slice(firstIdx, lastIdx), [data]);
 
   const [count, setCount] = useState<number>(0);
@@ -36,6 +44,7 @@ const Main = () => {
 
   useEffect(() => {
     searchParams.set("page", "1");
+    searchParams.set("status", "전체");
     setSearchParams(searchParams);
   }, []);
 
@@ -43,10 +52,29 @@ const Main = () => {
     window.scrollTo(0, 0);
   }, [currentPage]);
 
+  const handleStatus = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    searchParams.set("status", e.target.value);
+    setSearchParams(searchParams);
+  };
+
   return (
     <div className="page-container">
-      <span className="time-stamp">{count}초 전 업데이트</span>
-      <Pagination data={data ? data : []} />
+      <div className="section-wrapper">
+        <section>
+          <div className="select-wrapper">
+            <p>주문상태</p>
+            <select onChange={handleStatus}>
+              <option value="전체">전체</option>
+              <option value="완료">완료</option>
+              <option value="미완료">미완료</option>
+            </select>
+            <span className="time-stamp">{count}초 전 업데이트</span>
+          </div>
+        </section>
+        <section>
+          <Pagination data={data ? data : []} />
+        </section>
+      </div>
       <Table data={currentData} />
       <Pagination data={data ? data : []} />
     </div>
