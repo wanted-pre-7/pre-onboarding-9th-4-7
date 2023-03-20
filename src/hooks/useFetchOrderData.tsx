b53543d7094b5efeff74f2ff1ec4a4275805644e
violetwhenisawu <anashape710@gmail.com>
2023-03-20 23:26:14 +0900

    feat[#4 #5]: 오늘 거래건 날짜 상수화, 기본 정렬 ID 기준 오름차순으로 반환하도록 수정

--- src/hooks/useFetchOrderData.tsx
+++ src/hooks/useFetchOrderData.tsx
@@ -2,16 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchOrderData } from "../api/apis";
 import type { IOrderData } from "../types";
 
+const TODAY_DATE = "2023-03-08";
+
 const useFetchOrderData = () => {
   return useQuery(["orderData"], fetchOrderData, {
     refetchInterval: 5000,
     refetchOnWindowFocus: false,
     select: (data) => {
       const orderData: IOrderData[] = data.data?.filter((v: IOrderData) =>
-        v.transaction_time.includes("2023-03-08"),
+        v.transaction_time.includes(TODAY_DATE),
       );
 
-      return orderData;
+      return orderData.sort((a, b) => a.id - b.id);
     },
   });
 };
