d9e09477820464653f53d3a78648684752e82b91
che-97 <rue015@gmail.com>
2023-03-21 20:15:15 +0900

    feat: sort 아이콘 생성 및 테이블 스타일 변경

--- src/components/orderlist/OrderTable.tsx
+++ src/components/orderlist/OrderTable.tsx
@@ -1,4 +1,6 @@
 import type { IDataResponse } from "../../types";
+import TableSortAscIcon from "./TableSortAscIcon";
+import TableSortDescIcon from "./TableSortDescIcon";
 
 const OrderTable = ({
   data,
@@ -15,26 +17,51 @@ const OrderTable = ({
     <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
       <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
         <thead className="bg-gray-50 dark:bg-gray-800">
-          <tr className="flex w-full mb-4">
-            <th className="p-4 w-1/4" onClick={() => setSortConfig("id")}>
-              주문번호
-              {sortKey === "id" ? (
-                <span>{sortDir === "asc" ? " ⬇︎" : " ⬆︎"}</span>
-              ) : null}
+          <tr className="flex w-full">
+            <th
+              scope="col"
+              onClick={() => setSortConfig("id")}
+              className="p-4 w-1/4 rtl:text-right text-gray-500 dark:text-gray-400"
+            >
+              <button className="flex items-center gap-x-3 focus:outline-none">
+                <span>주문번호</span>
+                {sortKey === "id" ? (
+                  sortDir === "asc" ? (
+                    <TableSortAscIcon />
+                  ) : (
+                    <TableSortDescIcon />
+                  )
+                ) : null}
+              </button>
             </th>
             <th
-              className="p-4 w-1/4"
+              scope="col"
               onClick={() => setSortConfig("transaction_time")}
+              className="p-4 w-1/4 rtl:text-right text-gray-500 dark:text-gray-400"
             >
-              거래일 & 거래시간
-              {sortKey === "transaction_time" ? (
-                <span>{sortDir === "asc" ? " ⬇︎" : " ⬆︎"}</span>
-              ) : null}
+              <button className="flex items-center gap-x-3 focus:outline-none">
+                <span>거래일 & 거래시간</span>
+                {sortKey === "transaction_time" ? (
+                  sortDir === "asc" ? (
+                    <TableSortAscIcon />
+                  ) : (
+                    <TableSortDescIcon />
+                  )
+                ) : null}
+              </button>
             </th>
-            <th className="p-4 w-1/4">주문처리상태</th>
-            <th className="p-4 w-1/4">고객번호</th>
-            <th className="p-4 w-1/4">고객이름</th>
-            <th className="p-4 w-1/4">가격</th>
+            <th className="p-4 w-1/4 rtl:text-right text-gray-500 dark:text-gray-400">
+              주문처리상태
+            </th>
+            <th className="p-4 w-1/4 rtl:text-right text-gray-500 dark:text-gray-400">
+              고객번호
+            </th>
+            <th className="p-4 w-1/4 rtl:text-right text-gray-500 dark:text-gray-400">
+              고객이름
+            </th>
+            <th className="p-4 w-1/4 rtl:text-right text-gray-500 dark:text-gray-400">
+              가격
+            </th>
           </tr>
         </thead>
         <tbody className="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full h-96">
@@ -43,7 +70,15 @@ const OrderTable = ({
               <td className="p-4 w-1/4">{item.id}</td>
               <td className="p-4 w-1/4 text-center">{item.transaction_time}</td>
               <td className="p-4 w-1/4 text-center">
-                {item.status ? "완료" : "미완료"}
+                {item.status ? (
+                  <span className="bg-green-200 text-green-600 py-1 px-3 rounded-full text-xs">
+                    완료
+                  </span>
+                ) : (
+                  <span className="bg-red-200 text-red-600 py-1 px-3 rounded-full text-xs">
+                    미완료
+                  </span>
+                )}
               </td>
               <td className="p-4 w-1/4 text-center">{item.customer_id}</td>
               <td className="p-4 w-1/4 text-center">{item.customer_name}</td>
--- src/components/orderlist/Pagination.tsx
+++ src/components/orderlist/Pagination.tsx
@@ -13,34 +13,36 @@ const Pagination = ({
 
   return (
     <>
-      <nav className="flex justify-center items-center gap-1 m-4">
-        <button
-          className="border-none rounded-lg p-2 m-0 bg-black text-white text-base hover:bg-orange-600 disabled:bg-gray-400 disabled:cursor-not-allowed "
-          onClick={() => setPage(String(Number(page) - 2))}
-          disabled={page === "1"}
-        >
-          &lt;
-        </button>
-        {Array(numPages)
-          .fill(0)
-          .map((_, i) => (
-            <button
-              className="border-none rounded-lg p-2 m-0 bg-black text-white text-base hover:bg-orange-600 aria-selected:bg-orange-600 "
-              key={page + (i + 1)}
-              onClick={() => setPage(i + "")}
-              aria-selected={page == String(i + 1) ? true : false}
-            >
-              {i + 1}
-            </button>
-          ))}
-        <button
-          className="border-none rounded-lg p-2 m-0 bg-black text-white text-base hover:bg-orange-600 disabled:bg-gray-400 disabled:cursor-not-allowed"
-          onClick={() => setPage(page)}
-          disabled={page == numPages + ""}
-        >
-          &gt;
-        </button>
-      </nav>
+      {total > 0 ? (
+        <nav className="flex justify-center items-center gap-1 m-4">
+          <button
+            className="border-none rounded-lg p-2 m-0  text-black text-base disabled:cursor-not-allowed "
+            onClick={() => setPage(String(Number(page) - 2))}
+            disabled={page === "1"}
+          >
+            &lt;
+          </button>
+          {Array(numPages)
+            .fill(0)
+            .map((_, i) => (
+              <button
+                className="border-none rounded-3xl p-2 m-0  text-black text-base hover:bg-orange-100 aria-selected:bg-orange-100 "
+                key={page + (i + 1)}
+                onClick={() => setPage(i + "")}
+                aria-selected={page == String(i + 1) ? true : false}
+              >
+                {i + 1}
+              </button>
+            ))}
+          <button
+            className="border-none rounded-lg p-2 m-0  text-black text-base  disabled:cursor-not-allowed"
+            onClick={() => setPage(page)}
+            disabled={page == numPages + ""}
+          >
+            &gt;
+          </button>
+        </nav>
+      ) : null}
     </>
   );
 };
