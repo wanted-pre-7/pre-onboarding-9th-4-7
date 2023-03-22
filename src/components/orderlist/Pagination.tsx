4bf26b46077da33a5a66f1d705ef800112050845
che-97 <rue015@gmail.com>
2023-03-22 18:31:04 +0900

    feat: pagination에서 page 5개 단위로 분할하기

--- src/components/orderlist/Pagination.tsx
+++ src/components/orderlist/Pagination.tsx
@@ -1,15 +1,20 @@
-import { getNumPages } from "../../utils/pageUtil";
+import {
+  getNumPages,
+  getPageArrayIndex,
+  sliceArrayByLimit,
+} from "../../utils/pageUtil";
 
-const Pagination = ({
-  total,
-  page,
-  setPage,
-}: {
+export interface IPaginationProps {
   total: number;
   page: string;
   setPage: (value: string) => void;
-}) => {
-  const numPages = getNumPages(total);
+}
+
+const Pagination = ({ total, page, setPage }: IPaginationProps) => {
+  const pageArray = sliceArrayByLimit(getNumPages(total));
+  const pageArrayIndex = getPageArrayIndex(page);
+  const pages =
+    pageArray[pageArrayIndex > pageArray.length - 1 ? 0 : pageArrayIndex];
 
   return (
     <>
@@ -17,29 +22,61 @@ const Pagination = ({
         <nav className="flex justify-center items-center gap-1 m-4">
           <button
             className="border-none rounded-lg p-2 m-0  text-black text-base disabled:cursor-not-allowed "
+            onClick={() =>
+              setPage(
+                String(
+                  pageArray[pageArrayIndex === 0 ? 0 : pageArrayIndex - 1][0],
+                ),
+              )
+            }
+            disabled={pageArrayIndex === 0}
+            data-testid="test-left-button"
+          >
+            &lt;&lt;
+          </button>
+          <button
+            className="border-none rounded-lg p-2 m-0  text-black text-base disabled:cursor-not-allowed "
             onClick={() => setPage(String(Number(page) - 2))}
             disabled={page === "1"}
+            data-testid="test-left-button"
           >
             &lt;
           </button>
-          {Array(numPages)
-            .fill(0)
-            .map((_, i) => (
-              <button
-                className="border-none rounded-3xl p-2 m-0  text-black text-base hover:bg-orange-100 aria-selected:bg-orange-100 "
-                key={page + (i + 1)}
-                onClick={() => setPage(i + "")}
-                aria-selected={page == String(i + 1) ? true : false}
-              >
-                {i + 1}
-              </button>
-            ))}
+          {pages?.map((n) => (
+            <button
+              className="border-none rounded-3xl p-2 m-0  text-black text-base hover:bg-orange-100 aria-selected:bg-orange-100 "
+              key={page + (n + 1)}
+              onClick={() => setPage(n + "")}
+              aria-selected={page == String(n + 1) ? true : false}
+            >
+              {n + 1}
+            </button>
+          ))}
           <button
             className="border-none rounded-lg p-2 m-0  text-black text-base  disabled:cursor-not-allowed"
             onClick={() => setPage(page)}
-            disabled={page == numPages + ""}
+            disabled={pageArrayIndex === pageArray.length - 1}
+            data-testid="test-right-button"
           >
             &gt;
+          </button>
+          <button
+            className="border-none rounded-lg p-2 m-0  text-black text-base  disabled:cursor-not-allowed"
+            onClick={() =>
+              setPage(
+                String(
+                  pageArray[
+                    pageArrayIndex === pageArray.length - 1
+                      ? pageArrayIndex
+                      : pageArrayIndex + 1
+                  ][0],
+                ),
+              )
+            }
+            disabled={pageArrayIndex === pageArray.length - 1}
+            data-testid="test-right-button"
+          >
+            &gt;&gt;
           </button>
         </nav>
       ) : null}
--- src/utils/pageUtil.ts
+++ src/utils/pageUtil.ts
@@ -1,4 +1,5 @@
 export const pageLimit = 50;
+export const limit = 5;
 
 export const getNumPages = (total: number) => {
   return Math.ceil(total / pageLimit);
@@ -14,4 +15,17 @@ export const getStartNumber = (page: string | null) => {
 
 export const getEndNumber = (start: number) => {
   return start + pageLimit;
+};
+
+export const sliceArrayByLimit = (totalPage: number) => {
+  const totalPageArray = Array(totalPage)
+    .fill(0)
+    .map((_, i) => i);
+  return Array(Math.ceil(totalPage / limit))
+    .fill(0)
+    .map(() => totalPageArray.splice(0, limit));
+};
+
+export const getPageArrayIndex = (page: string) => {
+  return Math.floor(Number(page) / 5) - (Number(page) % 5 === 0 ? 1 : 0);
 };
