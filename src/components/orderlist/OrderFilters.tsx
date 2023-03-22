f44047ff03de3fd737ff5b0341cde9ea58c5c33c
che-97 <rue015@gmail.com>
2023-03-22 11:36:49 +0900

    feat: searchIcon 추가

--- src/components/orderlist/OrderFilters.tsx
+++ src/components/orderlist/OrderFilters.tsx
@@ -1,3 +1,5 @@
+import SearchIcon from "./SearchIcon";
+
 const OrderFilters = ({
   status,
   customer_name,
@@ -31,20 +33,7 @@ const OrderFilters = ({
       </div>
       <div className="relative flex items-center mt-4 md:mt-0">
         <span className="absolute">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
+          <SearchIcon />
         </span>
         <input
           onChange={handleChangeInput}
